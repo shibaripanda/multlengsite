e85e7c837f6d5027b88970bb5926dc92308edfc7
Ваше Имя <prevetlunatikam@gmail.com>
2025-05-02 13:22:33 +0300

    vill

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,31 +2,26 @@ import type { MetadataRoute } from 'next'
 import { lengList } from '../leng.config'
 
 const baseUrl = process.env.NEXT_PUBLIC_LINK
-// Список путей для генерации (добавь свои нужные страницы)
-// const staticPaths = ['', '/about', '/blog']
-const staticPaths = ['']
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const now = new Date()
 
-  const sitemap: MetadataRoute.Sitemap = []
-
-  for (const locale of lengList) {
-    // for (const path of staticPaths) {
-      const fullUrl = `${baseUrl}/${locale}`
-
-      sitemap.push({
-        url: fullUrl,
-        lastModified: now,
-        // changeFrequency: path === '' ? 'monthly' : 'yearly',
-        // priority: path === '' ? 1 : 0.7,
-        alternates: lengList.map((altLang) => ({
-          hreflang: altLang,
-          url: `${baseUrl}/${altLang}`,
-        })),
-      })
-    // }
+  const languagesUrlList = (lengList: string[]) => {
+    let res: {[key: string]: string} = {}
+    for(const i of lengList){
+      res[i] = baseUrl + `/${i}`
+    }
+    return res
   }
+
+  const sitemap: MetadataRoute.Sitemap = [{
+      url: baseUrl ?? 'http://localhost:4000',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+      alternates: {
+        languages: languagesUrlList(lengList),
+      }
+    }]
 
   return sitemap
 }
